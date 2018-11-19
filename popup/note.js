fe547516b2a6312106c3d41fe8d4d5a395880775
i-am-g2 <2017csb1083@iitrpr.ac.in>
2018-11-19 21:10:53 +0530

    Final Codes

--- popup/note.js
+++ popup/note.js
@@ -1,15 +1,22 @@
-function save() {
-	x = document.getElementById("inputText");	
-	toWrite = x.value;
-	inKey = document.URL;
-	
-	localStorage.setItem(inKey,toWrite);
+document.getElementById("body").onload = function()
+{
+    
+    let glbUrl;
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        // glbUrl = url;
+        document.getElementById("inputText").innerHTML = localStorage.getItem(url);
+        // return url;
+        // document.getElementById("inputText").innerHTML = url;	
+    }); 
 }
 
-function readV () {
-	x = document.getElementById("inputText")
-	temp = localStorage.getItem(document.URL)
-	x.value = temp  
+document.getElementById("btn1").onclick =   function(){
+    urlEx = temp = chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        // document.getElementById("inputText").innerHTML = url;	
+        var x = document.getElementById("inputText").value;
+        localStorage.setItem(url,x);
+    });
+    // document.getElementById("inputText").value = " ";
 }
-
-
